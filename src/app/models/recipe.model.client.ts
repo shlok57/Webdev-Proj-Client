ebe7e6de44f80a157894a16027ee9cf60aac4639
Gaurav Gandhi <gaurav.g101@gmail.com>
2019-04-19 00:21:32 -0400

    Added Description attribute to recipe and created a base html structure for recipe

--- src/app/models/recipe.model.client.ts
+++ src/app/models/recipe.model.client.ts
@@ -1,10 +1,11 @@
 //TODO moderator should have user object
 export class Recipe {
   id: number;
+  Description: String;
   Name: string;
   Date_Created: Date;
   Moderator : number;
-  Ingredients: string;
+  Ingredients: [string];
   Steps: [string];
   Image_Url: string;
   Video_Url: string;
